f847116ba80754b0bd468e5547d7ad71f60983ea
chao <chao@chaos-MacBook-Air.local>
2025-10-01 11:32:32 -0700

    Initial commit - working version

---
+++ components/AuthForm.tsx
@@ -0,0 +1,99 @@
+"use client"
+
+import { z } from "zod"
+import React from 'react'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import Image from "next/image" // <-- ADD THIS
+import Link from "next/link" // <-- ADD THIS
+
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+})
+
+
+
+
+const AuthForm = ( {type}: {type: FormType} ) => {
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+        username: "",
+        },
+    })
+    
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+    }
+
+    const isSignIn = type === 'sign-in';
+
+    return (
+        <div className="card-border lg:min-w-[566px]">
+            <div className="flex flex-col gap-6 card py-14 px-10">
+                <div className="flex flex-row gap-2 justify-center">
+                    <Image src="/logo_color_interview.svg" alt="logo" height={32} width={38} />
+
+                    <h2 className="text-primary-100">CX Prep </h2>
+                </div>
+
+                <h3> Simulate Your Job Interview with AI</h3>
+            
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
+                        {/* <FormField
+                        control={form.control}
+                        name="username"
+                        render={({ field }) => (
+                            <FormItem>
+                            <FormLabel>Username</FormLabel>
+                            <FormControl>
+                                <Input placeholder="shadcn" {...field} />
+                            </FormControl>
+                            <FormDescription>
+                                This is your public display name.
+                            </FormDescription>
+                            <FormMessage />
+                            </FormItem>
+                        )}
+                        /> */}
+
+                        { !isSignIn && <p>Name</p>}
+                        <p>Email</p>
+                        <p>Password</p>
+                        <Button className="btn" type="submit">{isSignIn ? 'Sign in' : 'Create an Account'}</Button>
+                    </form>
+                </Form>
+
+                <p className="text-center">
+                    {isSignIn ? 'No account yet?' : 'Have an account already?'}
+                    <Link href={!isSignIn ?  '/sign-in': '/sign-up' } className="font-bold text-red-500  ml-1" >
+                        {!isSignIn ? "sign in" : "Sign up"}
+                    </Link>
+                </p>
+            </div>
+            <div className="text-blue-500">Hello Tailwind</div>
+
+        </div>
+        
+  )
+}
+
+export default AuthForm
